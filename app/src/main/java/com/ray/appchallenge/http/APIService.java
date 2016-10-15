0d9ec4f39a5f4239f004aef535dbb30575bd2141
Raymond Chenon <raymond.chenon@gmail.com>
2016-10-16 00:55:34 +0200

    adapt Retrofit

--- app/src/main/java/com/ray/appchallenge/http/APIService.java
+++ app/src/main/java/com/ray/appchallenge/http/APIService.java
@@ -1,15 +1,16 @@
 package com.ray.appchallenge.http;
 
-import com.ray.appchallenge.dto.Msg;
-
 import java.util.List;
 
+import com.ray.appchallenge.dto.Msg;
+
 import retrofit2.Call;
+
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface APIService {
 
-    @GET("master/{id}.json")
+    @GET("{id}.json")
     Call<List<Msg>> getList(@Path("id") int page);
 }
--- app/src/main/java/com/ray/appchallenge/http/ApiServiceImpl.java
+++ app/src/main/java/com/ray/appchallenge/http/ApiServiceImpl.java
@@ -1,31 +1,30 @@
 package com.ray.appchallenge.http;
 
-import com.ray.appchallenge.dto.Msg;
-
 import java.util.List;
+
+import com.ray.appchallenge.dto.Msg;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
+
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
- * @author Raymond Chenon
+ * @author  Raymond Chenon
  */
 
 public class ApiServiceImpl {
 
     private APIService service;
 
-    public ApiServiceImpl(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://rawgit.com/zbsz/test_app/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+    public ApiServiceImpl() {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://rawgit.com/zbsz/test_app/master/")
+                                                  .addConverterFactory(GsonConverterFactory.create()).build();
 
         service = retrofit.create(APIService.class);
     }
 
-    public Call<List<Msg>> getList(final int page){
+    public Call<List<Msg>> getList(final int page) {
         return service.getList(page);
     }
 }
